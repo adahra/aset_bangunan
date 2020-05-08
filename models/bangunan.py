d1f9c3c66f533681844115b892cfbddb62ecbeb8
adahra <adnantoa@gmail.com>
2020-05-08 13:52:55 +0700

    Menambahkan tombol di model bangunan dan lantai

--- models/bangunan.py
+++ models/bangunan.py
@@ -1,10 +1,26 @@
-from odoo import fields
+from odoo import fields, api, _
 from . import zona
 
 
 class BangunanBangunan(zona.BangunanZona):
     _name = 'bangunan.bangunan'
     _description = 'Bangunan Bangunan Model'
+
+    @api.multi
+    def open_bangunan_lantai(self):
+        return {
+            'name': _('Lantai'),
+            'domain': [('bangunan_id', '=', self.id)],
+            'view_type': 'form',
+            'res_model': 'bangunan.lantai',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'type': 'ir.actions.act_window',
+        }
+
+    def get_lantai_count(self):
+        count = self.env['bangunan.lantai'].search_count([('bangunan_id', '=', self.id)])
+        self.lantai_count = count
 
     # name = fields.Char(string='Bangunan Nama', required=True)
     # kode = fields.Char(string='Bangunan Kode')
@@ -24,3 +40,4 @@ class BangunanBangunan(zona.BangunanZona):
     ], string='Bangunan Sertifikat', default='shm')
     nomor_sertifikat = fields.Text(string='Bangunan Nomor Sertifikat')
     keterangan = fields.Text(string='Bangunan Keterangan')
+    lantai_count = fields.Integer(string='Lantai', compute='get_lantai_count')
--- models/lantai.py
+++ models/lantai.py
@@ -1,4 +1,4 @@
-from odoo import fields
+from odoo import fields, api, _
 from . import bangunan
 
 
@@ -6,4 +6,21 @@ class BangunanLantai(bangunan.BangunanBangunan):
     _name = 'bangunan.lantai'
     _description = 'Bangunan Lantai Model'
 
+    @api.multi
+    def open_bangunan_ruang(self):
+        return {
+            'name': _('Ruang'),
+            # 'domain': [('patient_id', '=', self.id)],
+            'view_type': 'form',
+            'res_model': 'bangunan.ruang',
+            'view_id': False,
+            'view_mode': 'tree,form',
+            'type': 'ir.actions.act_window',
+        }
+
+    def get_ruang_count(self):
+        count = self.env['bangunan.ruang'].search_count([])
+        self.ruang_count = count
+
     bangunan_id = fields.Many2one('bangunan.bangunan', string='Bangunan ID')
+    ruang_count = fields.Integer(string='Ruang', compute='get_ruang_count')
