84911dafbc01fafc00d13296bca8586b7c19baf7
adahra <adnantoa@gmail.com>
2020-05-12 14:39:46 +0700

    Menambahkan fungsi inherit dari model untuk create sequence

--- models/area.py
+++ models/area.py
@@ -1,9 +1,18 @@
-from odoo import fields, models
+from odoo import fields, models, api, _
 
 
 class BangunanArea(models.Model):
     _name = 'bangunan.area'
     _description = 'Bangunan Area Model'
+    _rec_name = 'name'
+
+    @api.model
+    def create(self, vals):
+        if vals.get('kode', _('New')) == _('New'):
+            vals['kode'] = self.env['ir.sequence'].next_by_code('bangunan.area.sequence') or _('New')
+        return super(BangunanArea, self).create(vals)
 
     name = fields.Char(string='Bangunan Area Nama', required=True)
-    kode = fields.Char(string='Bangunan Area Kode')
+    # kode = fields.Char(string='Bangunan Area Kode')
+    kode = fields.Char(string='Kode', required=True, copy=False,
+                       readonly=True, index=True, default=lambda self: _('New'))
